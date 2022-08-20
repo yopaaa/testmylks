a541e4cc98f8f2ed7411672a5a4fbc36ca8b985b
yopaaa <pusi.ng027@gmail.com>
2022-08-20 11:37:01 +0700

    table

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // --------------------IMPORT------------------
 import express from 'express';
-import path from 'path';
 import mysql from 'mysql';
 import fs from 'fs';
 //import { myFunction } from './js/myFunction.js';
@@ -13,11 +12,11 @@ import fs from 'fs';
 
 // --------------------VARIABLE------------------
 const app = express();
-const __dirname = path.resolve();
 const config = JSON.parse(fs.readFileSync("./data/config.json", 'utf8'));
 const port = config.port;
 const host = config.host;
-const sql = 'SELECT * FROM user_details'
+
+
 
 
 const connection = mysql.createConnection(config.sql_database);
@@ -28,37 +27,52 @@ connection.connect(err => {
 });
 
 
+app.set('view engine', 'ejs')
 
 
     
 // --------------------FUNCTION------------------
 app.get('/',(req,res) => {
-    res.sendFile('./souce/index.html', {root: __dirname})
+    res.render('home', {
+        title: 'home',
+        })
+   
 })
 
 app.get('/about',(req,res) => {
     const url = req.url
     console.log(url); 
     
-    res.sendFile('./souce/about.html', {root: __dirname})
+    res.render('about')
 })
 
 app.get('/wibu',(req,res) => {
-    res.sendFile('./souce/wibu.html', {root: __dirname})
+    res.render('wibu')
 })
 
-app.get('/mysql',(req,res) => {
-    connection.query(sql, function (err, result) {
-        if (err) throw err;
-        res.json(result)
-        console.log(result.length);
-    });
+app.get('/database',(req,res) => {
+    switch (req.query.db) {
+        case 'sql':
+            connection.query('SELECT * FROM user_details', function (err, result) {
+                if (err) throw err;
+        
+                res.render('database/sql', {
+                  title: 'sql database',
+                  data:result
+                  })
+            }); 
+            break;
+    
+        default:
+            res.render('database')
+            break;
+    }
 })
 
 
 app.use('/',(req,res)=>{
     res.status(404);
-    res.sendFile('./souce/error.html', {root: __dirname})
+    res.render('error')
 })
 
 
