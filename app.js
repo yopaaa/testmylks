9d886f29b63e02a45bbf081a73b610b8f33f6e79
yopaaa <pusi.ng027@gmail.com>
2022-08-22 10:41:14 +0700

    awok

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import mysql from 'mysql';
 import fs from 'fs';
-//import { myFunction } from './js/myFunction.js';
+//import { myFunction } from './js/myFunction.js';k
 
 
 
